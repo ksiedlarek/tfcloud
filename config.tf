adce9d7ffad64724546bd4be2c0e0832372d1958
Kasia Siedlarek <kasia@siedlarek.net>
2021-12-02 21:28:33 +0100

    initial commit

---
+++ config.tf
@@ -0,0 +1,11 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.2.0"
+    }
+  }
+}
+
+provider "google" {
+}
---
+++ main.tf
@@ -0,0 +1,7 @@
+resource "random_pet" "name" {
+    length = 2
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = random_pet.name.id
+}
